70707a2d83a3c3f9a928b8b0fdfd545fafcb5d2b
Ayush Mahto <ayushpersonal69@gmail.com>
2026-09-15 20:39:21 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.19%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -4,7 +4,7 @@ public:
         
         int count = 0;
         for(int i = s.size() - 1;i >= 0; i--) {
-            if ( s[i] != ' ' ) {
+            if (s[i] != ' ' ) {
                 count ++;
             }
        else if(count > 0) {
