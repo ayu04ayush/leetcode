7737a70073541e0a7f08e265f6a7b6ddee5edd34
Ayush Mahto <ayushpersonal69@gmail.com>
2026-09-25 22:05:46 +0530

    Time: 0 ms (100%), Space: 11.6 MB (60.29%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+ for(int i = digits.size()- 1 ; i >= 0;i--) {
+    if( digits[i]  < 9 ) { 
+        digits[i] = digits[i] + 1;
+        return digits;  
+    }
+ 
+  digits[i] = 0;
+ }
+ vector<int> ans(digits.size() + 1);
+    ans[0] = 1;
+    
+    return ans;
+     }
+};
